625f74f480e2ee8ffecea5d6546a6e1d7a55f16a
sirpah <hadasirpa@gmail.com>
2025-03-10 18:00:01 +0545

    Login using pom

--- LearningSelenium-ExtraPractice/Login.py
+++ LearningSelenium-ExtraPractice/Login.py
@@ -1,18 +1,15 @@
-
-from sys import executable
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 
 class login():
     def loginpage(self):
-        options= webdriver.ChromeOptions()
-        options.add_experimental_option("detach",True)
-        driver=webdriver.Chrome(options=options)
+        # options= webdriver.ChromeOptions()
+        # options.add_experimental_option("detach",True)
+        # driver=webdriver.Chrome(options=options)
+        driver = webdriver.Chrome()
 
-        driver.get("https://foodmandu.com")
+        driver.get("https://foodmandu.com/")
         # driver.find_element(By.NAME,'Email').send_keys('example@gmail.com')
         # driver.find_element(By.NAME,'Password').send_keys('12345678')
         # driver.find_element(By.CSS_SELECTOR, 'input[type=\"checkbox\"] + label').click()
@@ -20,7 +17,7 @@ class login():
         driver.find_element(By.CSS_SELECTOR, '.input-group .form-control:not(:last-child), .input-group-addon:not(:last-child), .input-group-btn:not(:last-child) > .btn, .input-group-btn:not(:last-child) > .btn-group > .btn, .input-group-btn:not(:last-child) > .dropdown-toggle, .input-group-btn:not(:first-child) > .btn:not(:last-child):not(.dropdown-toggle), .input-group-btn:not(:first-child) > .btn-group:not(:last-child) > .btn').send_keys('Korean')
         driver.find_element(By.CSS_SELECTOR, '.hero__container button.btn').click()
 
-
+        time.sleep(5)
 
 findbyname= login()
 findbyname.loginpage()
